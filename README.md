d86bae0058ee8883a886818d1104b956da392e34
TrueEndeavor <lathaannur@gmail.com>
2025-11-18 17:20:57 +0100

    feat: add step-by-step flashcard generation logic

--- README.md
+++ README.md
@@ -4,3 +4,48 @@ This repository is for
 - learning
 - testing small ideas and
 - building basic AI workflows step-by-step
+
+## Project 1: 'gemini-hello.py' (Completed) ✅
+    My first gemini API integration
+
+### Learnings:
+    - How to use Gemini API, Models
+    - Error handling
+    - Reading from external file
+
+### How to use:
+    ```bash
+    export GEMINI_API_KEY="key-here"
+    echo "What is <question>?" > prompt.txt
+    python gemini-hello.py
+    ```
+
+## Project 2: 'flashcard-gen.py' (In progress) 🚧
+
+Building a flashcard generator to learn multi-step AI workflows.
+
+    The Flow
+        Topic (from prompt.txt)
+        ↓
+        Generate Questions (Phase 2 - TODO)
+        ↓
+        Generate Answers (Phase 3 - TODO)
+        ↓
+        Save Flashcards (Phase 4 - TODO)
+
+### Learnings:
+    - Manual workflow orchestration (the hard way!)
+    - Will learn: Why LangGraph exists (after feeling the pain)
+
+### Current Status:
+    - ✅ Phase 1: Basic structure & file reading
+    - 📋 Phase 2: Generate questions (next)
+    - 📋 Phase 3: Generate answers
+    - 📋 Phase 4: Save flashcards
+
+### How to use:
+    ```bash
+    export GEMINI_API_KEY="key-here"
+    echo "Photosynthesis" > prompt.txt
+    python flashcard-gen.py
+    ```
---
+++ flashcard-gen.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+MODEL_NAME = "gemini-2.5-flash"
+
+def ask_gemini(prompt: str)->str:
+	if not os.environ.get("GEMINI_API_KEY"):
+		raise ValueError(
+			"GEMINI_API_KEY not found."
+			"Set it in the env: export GEMINI_API_KEY='api-key'"
+		)
+	try:
+		model = genai.GenerativeModel(MODEL_NAME)
+		result = model.generate_content(prompt)
+		return result.text
+	except Exception as e:
+		print(f"\nERROR: {str(e)}")
+
+		if "404" in str(e):
+			print(f"ERROR: Model '{MODEL_NAME}' not found. Correct the model name")
+		if "429" in str(e):
+			print(f"ERROR: You've hit the rate limit of Gemini")
+		raise
+
+if __name__ == "__main__":
+	try:
+		with open("prompt.txt", "r") as f:
+			user_prompt = f.read().strip()
+		print(f"Reading from prompt.txt")
+	except FileNotFoundError:
+		print("ERROR: prompt.txt not found")
+		print("Create prompt.txt with your question and try again")
+		sys.exit(1)
+
+	if not user_prompt:
+		print("ERROR: prompt.txt is empty")
+		sys.exit(1)
+		
+	#answer = ask_gemini(user_prompt)
+	
+	#print("PROMPT: ", user_prompt)
+	#print("GEMINI'S ANSWER: ", answer)
+
+	print("Basic Flashcard Genertor")
+	# TODO: Generate Questions
+	# TODO: Generate Answers
+	# TODO: Save flashcards in file
+
+	print("\nDone")
